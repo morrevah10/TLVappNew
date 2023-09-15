eead1579332efc953342ea5531b4991baf976aef
Mor Revah <morrevah10@gmail.com>
2023-09-16 02:50:30 +0300

    logo router

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../srvices/auth.service';
 import { User } from '../models/user.model';
-
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/srvices/user.service';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,31 @@ export class AppComponent implements OnInit {
   currentUser: User | null = null;
   opened = false;
   isAuthenticated = false;
+  user: any;
 
 
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
 
-  constructor(private authService: AuthService,
-    ) {
-    }
+  ) {}
 
   ngOnInit() {
     this.authService.isAuthenticated$.subscribe((isAuthenticated) => {
       this.isAuthenticated = isAuthenticated;
     });
+    this.userService.user$.subscribe((user) => {
+      console.log('User updated:', user);
+      this.user = user;
+    });
   }
-
-
- 
+  onLogoClicked() {
+    if(this.user){
+      this.router.navigate(['/home']);
+    }
+    else{
+      this.router.navigate(['/login']);
+    }
+  }
 }
